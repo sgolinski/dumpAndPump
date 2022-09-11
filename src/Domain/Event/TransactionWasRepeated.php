6c839d01af16ae777f733b9e125fd51c40412d4d
sgolinski <simon.golinski@gmail.com>
2022-09-11 13:48:34 +0200

    add transaction was repeated event

--- src/Domain/Event/TransactionWasRepeated.php
+++ src/Domain/Event/TransactionWasRepeated.php
@@ -3,17 +3,36 @@
 namespace App\Domain\Event;
 
 use App\Domain\ValueObjects\ExchangeChain;
-use App\Domain\ValueObjects\Id;
-use App\Domain\ValueObjects\Name;
 use App\Domain\ValueObjects\Price;
 use App\Infrastructure\DomainEvent;
 use DateTimeImmutable;
 
+
 class TransactionWasRepeated implements DomainEvent
 {
+    private Price $price;
+    private ExchangeChain $exchangeChain;
+
+    public function __construct(Price $price, ExchangeChain $chain)
+    {
+        $this->price = $price;
+        $this->exchangeChain = $chain;
+    }
 
     public function occurredOn(): DateTimeImmutable
     {
         return new DateTimeImmutable();
     }
+
+    public function exchangeChain(): ExchangeChain
+    {
+        return $this->exchangeChain;
+    }
+
+
+    public function price(): Price
+    {
+        return $this->price;
+    }
+
 }
